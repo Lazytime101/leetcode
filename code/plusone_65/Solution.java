126d0d7f6a9546be78b1618123f892cef6e4da83
Lazytime101 <lion2017s@outlook.com>
2018-09-14 23:21:17 +0800

    test

--- code/plusone_65/Solution.java
+++ code/plusone_65/Solution.java
@@ -3,6 +3,7 @@ package plusone_65;
 public class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
+        int y = 1;
         //从末尾开始累加，如果本位为9 进位，小于9加1 返回
         for (int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] < 9) {

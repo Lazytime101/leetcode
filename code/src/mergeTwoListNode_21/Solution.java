8586174f3ae9cc92dff83769fe6bea125d4b1d41
Lazytime101 <601978920@qq.com>
2019-06-16 18:37:56 +0800

    update

--- code/src/mergeTwoListNode_21/Solution.java
+++ code/src/mergeTwoListNode_21/Solution.java
@@ -34,4 +34,17 @@ public class Solution {
         }
         return result.next;
     }
+
+    //递归
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2){
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        if(l1.val < l2.val){
+            l1.next = mergeTwoLists2(l1.next, l2);
+            return l1;
+        } else{
+            l2.next = mergeTwoLists2(l1, l2.next);
+            return l2;
+        }
+    }
 }

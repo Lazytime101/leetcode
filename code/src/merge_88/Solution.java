dfc31a09ab8350075ae099d90dec7204e9a1badb
lazytime101 <lion2017s@outlook.com>
2020-06-03 00:30:23 +0800

    优化代码减少变量

--- code/src/merge_88/Solution.java
+++ code/src/merge_88/Solution.java
@@ -4,42 +4,32 @@ public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
         int[] temp = new int[m];
-        for (int i = 0; i <= m-1; i++) {
+        //copy array
+        for (int i = 0; i <= m - 1; i++) {
             temp[i] = nums1[i];
         }
         int a = 0;
         int b = 0;
         int end = 0;
-        int curr1 = 0;
-        int curr2 = 0;
-        int result = 0;
 
         while (a <= m - 1 || b <= n - 1) {
-
-            if (a <= m - 1) {
-                 curr1 = temp[a];
-            } else {
-                result = nums2[b++];
-                nums1[end++] = result;
+            if (a > m - 1) {
+                nums1[end++] = nums2[b++];
                 continue;
             }
 
-            if (b <= n - 1) {
-                 curr2 = nums2[b];
-
-            } else {
-                result = temp[a++];
-                nums1[end++] = result;
+            if (b > n - 1) {
+                nums1[end++] = temp[a++];
                 continue;
             }
-            if(curr1<=curr2){
-                nums1[end++] = curr1;
+
+            if (temp[a] <= nums2[b]) {
+                nums1[end++] = temp[a];
                 a++;
-            }else {
-                nums1[end++] = curr2;
+            } else {
+                nums1[end++] = nums2[b];
                 b++;
             }
         }
-
     }
 }

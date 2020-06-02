737da099861bdd3801ca09557e01d7ed54239f3d
lazytime101 <lion2017s@outlook.com>
2020-06-03 00:38:18 +0800

    update

--- code/src/merge_88/Solution.java
+++ code/src/merge_88/Solution.java
@@ -32,4 +32,22 @@ public class Solution {
             }
         }
     }
+
+
+    /**
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     * 减少一次copy 数组
+     */
+    public void merge1(int[] nums1, int m, int[] nums2, int n) {
+        int tail1 = m - 1, tail2 = n - 1, finished = m + n - 1;
+        while (tail1 >= 0 && tail2 >= 0) {
+            nums1[finished--] = (nums1[tail1] >= nums2[tail2]) ? nums1[tail1--] : nums2[tail2--];
+        }
+        while (tail2 >= 0) {
+            nums1[finished--] = nums2[tail2--];
+        }
+    }
 }
